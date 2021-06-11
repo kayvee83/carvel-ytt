1dafb884c6348c466d8f41461e3924b43e90a510
Dennis Leon <leonde@vmware.com>
2021-06-11 10:54:48 -0700

    Automate creating github release
    - Uses goreleaser to upload and generate draft release notes with assets
    attached, and preliminary release notes generated
    
    Authored-by: Dennis Leon <leonde@vmware.com>

--- hack/build-binaries.sh
+++ hack/build-binaries.sh
@@ -2,16 +2,21 @@
 
 set -e -x -u
 
+LATEST_GIT_TAG=$(git describe --tags | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
+VERSION="${1:-$LATEST_GIT_TAG}"
+
+go fmt ./cmd/... ./pkg/... ./test/...
+go mod vendor
+go mod tidy
+
 # makes builds reproducible
 export CGO_ENABLED=0
-repro_flags="-ldflags=-buildid= -trimpath"
+LDFLAGS="-X github.com/k14s/ytt/pkg/version.Version=$VERSION -buildid="
 
-./hack/build.sh
+GOOS=darwin GOARCH=amd64 go build -ldflags="$LDFLAGS" -trimpath -o ytt-darwin-amd64 ./cmd/ytt
+GOOS=darwin GOARCH=arm64 go build -ldflags="$LDFLAGS" -trimpath -o ytt-darwin-arm64 ./cmd/ytt
+GOOS=linux GOARCH=amd64 go build -ldflags="$LDFLAGS" -trimpath -o ytt-linux-amd64 ./cmd/ytt
+GOOS=linux GOARCH=arm64 go build -ldflags="$LDFLAGS" -trimpath -o ytt-linux-arm64 ./cmd/ytt
+GOOS=windows GOARCH=amd64 go build -ldflags="$LDFLAGS" -trimpath -o ytt-windows-amd64.exe ./cmd/ytt
 
-GOOS=darwin GOARCH=amd64 go build $repro_flags -o ytt-darwin-amd64 ./cmd/ytt
-GOOS=darwin GOARCH=arm64 go build $repro_flags -o ytt-darwin-arm64 ./cmd/ytt
-GOOS=linux GOARCH=amd64 go build $repro_flags -o ytt-linux-amd64 ./cmd/ytt
-GOOS=linux GOARCH=arm64 go build $repro_flags -o ytt-linux-arm64 ./cmd/ytt
-GOOS=windows GOARCH=amd64 go build $repro_flags -o ytt-windows-amd64.exe ./cmd/ytt
-
-shasum -a 256 ./ytt-*-*
+shasum -a 256 ./ytt-darwin-amd64 ./ytt-darwin-arm64 ./ytt-linux-amd64 ./ytt-linux-arm64 ./ytt-windows-amd64.exe
